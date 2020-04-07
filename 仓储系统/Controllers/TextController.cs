549f6ac40a95adc146103b8593c081776a0e257a
Administrator <Administrator@LUO>
2020-04-07 11:10:38 +0800

    添加项目文件。

---
+++ 仓储系统/Controllers/TextController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 仓储系统.Controllers
+{
+    public class TextController : Controller
+    {
+        // GET: Text
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 仓储系统/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="仓储系统.MvcApplication" Language="C#" %>
