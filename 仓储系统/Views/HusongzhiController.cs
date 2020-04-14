601a042bea2e715d746c9196803632fa0f33c6e3
松志 胡 <2386622016@qq.com>
2020-04-14 09:02:40 +0800

    胡

---
+++ 仓储系统/Views/HusongzhiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 仓储系统.Views
+{
+    public class HusongzhiController : Controller
+    {
+        // GET: Husongzhi
+        public ActionResult baosun()
+        {
+            return View();
+        }
+
+        public ActionResult yiku()
+        {
+            return View();
+        }
+
+        public ActionResult pandian()
+        {
+            return View();
+        }
+
+        public ActionResult tuihuo()
+        {
+            return View();
+        }
+    }
+}
