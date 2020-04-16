6b6136ecb464f06e46d1b50dcad948756bd9f0c2
ASUS <ASUS@>
2020-04-16 15:13:48 +0800

    yang

---
+++ 仓储系统/Controllers/YangWenJieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 仓储系统.Controllers
+{
+    public class YangWenJieController : Controller
+    {
+        // GET: YangWenJie
+        public ActionResult buMenGuanLi()
+        {
+            return View();
+        }
+
+        public ActionResult caiDanGuanLi()
+        {
+            return View();
+        }
+        public ActionResult quanXian()
+        {
+            return View();
+        }
+    }
+}
