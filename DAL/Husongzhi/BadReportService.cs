494359854b6b5225edfd90ca36bef9a71bf2e335
松志 胡 <2386622016@qq.com>
2020-04-16 17:01:18 +0800

    1

---
+++ DAL/Husongzhi/BadReportService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models1;
+namespace DAL
+{
+    public class BadReportService
+    {
+
+        public static IQueryable GetBadReport1()
+        {
+
+            WarehouseEntities entity = new WarehouseEntities();
+            var obj = from p in entity.BadReport
+                      orderby p.BadNum ascending
+                      select new
+                      {
+                          BadNum = p.BadNum,
+                          BadTypeId = p.BadTypeId,
+                          Num = p.Num,
+                          SumMoney = p.SumMoney,
+                          Status = p.Status,
+                          Operation = p.Operation,
+                          AuditUser = p.AuditUser,
+                          AuditTime = p.AuditTime,
+                          IsDelete = p.IsDelete,
+                          Remark = p.Remark
+                      };
+            return obj;
+        }
+
+        
+        public static PageList GetBadReport(int pageIndex, int pageSize)
+        {
+            //实例化分页类
+            PageList list = new PageList();
+
+            WarehouseEntities entity = new WarehouseEntities();
+            var obj = from p in entity.BadReport
+                      orderby p.BadNum ascending
+                      select new
+                      {
+                          BadNum = p.BadNum,
+                          BadTypeId = p.BadTypeId,
+                          Num = p.Num,
+                          SumMoney = p.SumMoney,
+                          Status = p.Status,
+                          Operation = p.Operation,
+                          AuditUser = p.AuditUser,
+                          AuditTime = p.AuditTime,
+                          IsDelete = p.IsDelete,
+                          Remark = p.Remark
+                      };
+            //设置分页数据
+            list.DataList = obj.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            //设置总页数
+            int rows = obj.Count();
+            list.PageCount = rows % pageSize == 0 ? rows / pageSize : rows / pageSize + 1;
+
+
+            return list;
+        }
+    }
+}
---
+++ Models1/PageList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models1
+{
+    public class PageList
+    {
+        public int PageCount { get; set; }
+        public IQueryable DataList { get; set; }
+    }
+}
