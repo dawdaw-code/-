6e4ad3f8e34dd08a6f033de77580f64abb1a4aa1
ASUS <ASUS@>
2020-04-17 12:32:58 +0800

    yang

---
+++ BLL/yangWenJie/DepartManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models1;
+using DAL;
+
+namespace BLL
+{
+    public class DepartManager
+    {
+        public static List<Depart> selectAll()
+        {
+            return DepartService.selectAll();
+        }
+    }
+}
---
+++ DAL/yangWenJie/DepartService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models1;
+
+namespace DAL
+{
+    public class DepartService
+    {
+        public static List<Depart> selectAll()
+        {
+            WarehouseEntities con = new WarehouseEntities();
+            var obj = from p in con.Depart select p;
+            return obj.ToList();
+        }
+    }
+}
