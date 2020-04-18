844efac719b7dbfaee26cb603eca84be6043c12a
ASUS <ASUS@DESKTOP-HD09DTF>
2020-04-18 11:24:59 +0800

    jjx

---
+++ 仓储系统/Controllers/JiaJiaXinController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 仓储系统.Controllers
+{
+    public class JiaJiaXinController : Controller
+    {
+        // GET: JiaJiaXin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult List2()
+        {
+            return View();
+        }
+
+
+        public ActionResult Rukuguanli()
+        {
+            return View();
+        }
+        public ActionResult Chukuguanli()
+        {
+            return View();
+        }
+        public ActionResult AddRukudan()
+        {
+            return View();
+        }
+        public ActionResult AddChukudan()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
