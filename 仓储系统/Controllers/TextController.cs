9199662fb8ebde1e8b8730ad0d47372c7a742f7b
Administrator <Administrator@LUO>
2020-04-24 14:58:43 +0800

    luo

--- 仓储系统/Controllers/TextController.cs
+++ 仓储系统/Controllers/TextController.cs
@@ -29,6 +29,15 @@ namespace 仓储系统.Controllers
         }
         public ActionResult Yuango()
         {
+            List<Role> list = YuangoManager.juesselect();
+            //id是隐藏值，name是显示值
+            list.Insert(0, new Role { Id = 0, RoleName = "请选择" });
+            ViewBag.RoleId = new SelectList(list, "Id", "RoleName");
+
+            List<Depart> list1 = YuangoManager.bumenselect();
+            //id是隐藏值，name是显示值
+            list1.Insert(0, new Depart { Id = 0, DepartName = "请选择" });
+            ViewBag.DepartId = new SelectList(list1, "Id", "DepartName");
             return View();
         }
         public ActionResult Juese()
@@ -53,22 +62,38 @@ namespace 仓储系统.Controllers
             }
 
         }
-        //新增
-        public ActionResult Add(Depart det)
-        {
-            return Json(YuangoManager.Add(det), JsonRequestBehavior.AllowGet);
-        }
-
-        //删除(修改标识Id)
-        public ActionResult delDepart(int Id)
+        public ActionResult delDepart( int id)
         {
             //det.IsDelete = 1;
-            return Json(YuangoManager.delDepart(Id), JsonRequestBehavior.AllowGet);
+            return Json(YuangoManager.delDepart( id), JsonRequestBehavior.AllowGet);
         }
-        //修改
-        public ActionResult upDepartById(string DepartName, int id)
+        public ActionResult adminid(int id)
         {
-            return Json(YuangoManager.upDepartById(DepartName, id), JsonRequestBehavior.AllowGet);
+            return Json(YuangoManager.adminid(id),JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult adminidup(string id, string UserName, string RealName, string Email, string Phone, int DepartId, int RoleId)
+        {
+            return Json(YuangoManager.adminidup(id, UserName, RealName, Email, Phone, DepartId, RoleId), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult getRole(int pageIndex, int pageSize, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                //根据部门名查询
+                return Json(jueseManager.getRoleName(pageIndex, pageSize, name), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                //页面加载
+                return Json(jueseManager.getRolePower(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        
+             public ActionResult delRole(int id)
+        {
+            //det.IsDelete = 1;
+            return Json(jueseManager.delRole(id), JsonRequestBehavior.AllowGet);
         }
     }
 }
