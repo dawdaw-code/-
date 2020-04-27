632e99b23a9ea21643da0eb0f17b50862e43173f
Administrator <Administrator@LUO>
2020-04-27 22:31:44 +0800

    luo

--- DAL/luo/YuangoService.cs
+++ DAL/luo/YuangoService.cs
@@ -32,32 +32,7 @@ namespace DAL.luo
             WarehouseEntities entities = new WarehouseEntities();
             PageList list = new PageList();
             var obj = from p in entities.Admin
-                      where p.UserName.IndexOf(name) != -1 && p.IsDelete == 0
-                      orderby p.Id
-                      select new
-                      {
-                          UserName = p.UserName,
-                          UserCode = p.UserCode,
-                          RealName = p.RealName,
-                          Email = p.Email,
-                          Phone = p.Phone,
-                          DepartId = p.Depart.DepartNum,
-                          RoleId = p.Role.RoleName
-                      };
-            list.DataList = obj.Skip((pageIndex - 1) * pageSize).Take(pageSize);
-            //设置总页数
-            int rows = obj.Count();
-            list.PageCount = rows; 
-
-            return list;
-        }
-        //页面加载
-        public static PageList getDepart(int pageIndex, int pageSize)
-        {
-            WarehouseEntities entities = new WarehouseEntities();
-            PageList list = new PageList();
-            var obj = from p in entities.Admin
-                      where p.IsDelete == 0
+                      where p.IsDelete==0
                       orderby p.Id
                       select new
                       {
@@ -70,13 +45,54 @@ namespace DAL.luo
                           DepartId = p.Depart.DepartNum,
                           RoleId = p.Role.RoleName
                       };
+            if (!string.IsNullOrEmpty(name))
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (!Char.IsNumber(name, i))
+                    {
+                        obj = obj.Where(item => item.UserName.IndexOf(name) != -1);
+                    }
+                    else
+                    {
+                        int Name = int.Parse(name);
+                        obj = obj.Where(item => item.UserName.IndexOf(name) != -1 || item.Id == Name);
+                    }
+                }
+            }
             list.DataList = obj.Skip((pageIndex - 1) * pageSize).Take(pageSize);
             //设置总页数
             int rows = obj.Count();
-            list.PageCount = rows; //rows % pageSize == 0 ? rows / pageSize : rows / pageSize + 1;
+            list.PageCount = rows; 
 
             return list;
         }
+        ////页面加载
+        //public static PageList getDepart(int pageIndex, int pageSize)
+        //{
+        //    WarehouseEntities entities = new WarehouseEntities();
+        //    PageList list = new PageList();
+        //    var obj = from p in entities.Admin
+        //              where p.IsDelete == 0
+        //              orderby p.Id
+        //              select new
+        //              {
+        //                  Id = p.Id,
+        //                  UserName = p.UserName,
+        //                  UserCode = p.UserCode,
+        //                  RealName = p.RealName,
+        //                  Email = p.Email,
+        //                  Phone = p.Phone,
+        //                  DepartId = p.Depart.DepartNum,
+        //                  RoleId = p.Role.RoleName
+        //              };
+        //    list.DataList = obj.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        //    //设置总页数
+        //    int rows = obj.Count();
+        //    list.PageCount = rows; //rows % pageSize == 0 ? rows / pageSize : rows / pageSize + 1;
+
+        //    return list;
+        //}
 
        
         public static List<Role> juesselect()
@@ -125,6 +141,25 @@ namespace DAL.luo
             WarehouseEntities entities = new WarehouseEntities();
             var obj = (from p in entities.Admin where p.Id == id select p).First();
             obj.IsDelete = 1;
+            return entities.SaveChanges();
+        }
+        //新增
+        public static int insert(string UserCode, string UserName, string RealName, string Email, string Phone, int DepartId, int RoleId)
+        {
+            WarehouseEntities entities = new WarehouseEntities();
+            var obj = new  Admin();
+            obj.UserName = UserName;
+            obj.UserCode = UserCode;
+            obj.RealName = RealName;
+            obj.Email = Email;
+            obj.Phone = Phone;
+            obj.DepartId = DepartId;
+            obj.RoleId = RoleId;
+            obj.CreateTime = DateTime.Now;
+            obj.Password = "123456";
+            obj.IsDelete = 0;
+            obj.LoginCount = 1;
+            entities.Admin.Add(obj);
             return entities.SaveChanges();
         }
     }
